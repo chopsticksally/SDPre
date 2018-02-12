81333a5bbc79b57799daeeb1a13060ac1988d177
Miranda Martin <miranda@Mirandas-MacBook-Pro.local>
2018-02-12 08:26:43 -0700

    exercise1 completed JTHW

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg {
     public static void main( String[] args ) {
-        System.out.println("I am determined to learn how to code");
-        System.out.println("Today's date is");
+        System.out.println("I, Miranda Martin, am determined to learn how to code");
+        System.out.println("Today's date is Feb. 12th 2018");
       }
 }
