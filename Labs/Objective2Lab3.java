02b39edeee35ad35b1ef8b4bf8b2872e0cdf8fa1
Miranda Martin <miranda@Mirandas-MacBook-Pro.local>
2018-02-12 14:24:31 -0700

    Obj2Lab3 completed

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String food1 , food2 , food3;
+    food1 = "Apple";
+    food2 = "Banana";
+    food3 = "Coconut";
+      System.out.println("My favorite foods are:\n1." + food1 +"\n2." + food2 +"\n3." + food3);
+
+  }
+}
