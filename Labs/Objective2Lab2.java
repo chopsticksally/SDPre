0794978d40246719777df6622f29c2e2d076a11c
Miranda Martin <miranda@Mirandas-MacBook-Pro.local>
2018-02-12 13:35:47 -0700

    Obj2Lab2 completed

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Miranda Martin ";
+    int age = 36;
+    String state = " California";
+      System.out.println(name + age + state);
+
+  }
+}
