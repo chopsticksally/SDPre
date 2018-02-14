3b4f339124c74217419d6b06d6c17560b49fe879
Miranda Martin <miranda@Mirandas-MacBook-Pro.local>
2018-02-13 21:56:32 -0700

    exercise9 completed JTHW

--- JavaTheHardWay/BMICalculator.java
+++ JavaTheHardWay/BMICalculator.java
@@ -3,15 +3,24 @@ import java.util.Scanner;
   public class BMICalculator {
     public static void main(String[] args) {
       Scanner keyboard = new Scanner(System.in);
-      double m, kg, bmi;
+      double inches, pounds, m, kg, bmi;
 
-      System.out.print(" Your height in m: ");
-      m = keyboard.nextDouble();
+      System.out.print(" Your height in inches: ");
+      inches = keyboard.nextDouble();
 
-      System.out.print(" Your weight in kg: ");
-      kg = keyboard.nextDouble();
+      System.out.print(" Your weight in pounds: ");
+      pounds = keyboard.nextDouble();
+
+      m = inches * .0254;
+      kg = pounds * 0.453592;
+
 
       bmi = kg / (m*m);
+// inch= 0.0254 meters
+// pound=0.453592 kgs
+
+
+
 
       System.out.println(" Your BMI is " + bmi);
 
