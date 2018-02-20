f9eb021db978df095e595a98d9c85e59475901bd
Miranda Martin <miranda@Mirandas-MacBook-Pro.local>
2018-02-19 19:45:37 -0700

    Obj6Lab2 complete

---
+++ Labs/Objective6Lab2.java
@@ -0,0 +1,16 @@
+public class Objective6Lab2 {
+  public static void main(String[] args) {
+    int counter = 0;
+
+
+    while ( counter <= 20) {
+      counter++;
+      if (counter <= 20) {
+        System.out.println(counter);
+    }
+  }
+     //while (counter < 20) {
+    // System.out.println( 1 + (counter++));
+
+  }
+}
