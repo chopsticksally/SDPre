5d895ff29be3c65a2faf07e4a9ed2513e5d6934c
Miranda Martin <miranda@Mirandas-MacBook-Pro.local>
2018-02-19 18:42:11 -0700

    exercise33 completerJTHW

---
+++ JavaTheHardWay/ThereAndBackAgain.java
@@ -0,0 +1,13 @@
+public class ThereAndBackAgain{
+  public static void main(String[] args) {
+    System.out.println("Here.");
+    erebor();
+    System.out.println("Back first time.");
+    erebor();
+    //erebor();
+    System.out.println("Back the second time.");
+  }
+  public static void erebor() {
+    System.out.println("There.");
+  }
+}
