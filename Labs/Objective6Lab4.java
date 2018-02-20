8ee92371d1705270e362211e2cc3b28ff04c16ff
Miranda Martin <miranda@Mirandas-MacBook-Pro.local>
2018-02-19 22:47:26 -0700

    Obj6Lab5 complete

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,16 @@
+public class Objective6Lab4 {
+  public static void main(String[] args){
+    int sum, counter;
+    counter = 0;
+    sum = 0;
+
+    while(counter < 20) {
+      counter++;
+      sum = sum + counter;
+
+}
+      System.out.println(sum);
+
+
+  }
+}
---
+++ Labs/Objective6Lab5.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Objective6Lab5 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+    //String selection;
+
+    while (true) {
+      System.out.println("_____Menu_____");
+      System.out.println("1: Say Hello");
+      System.out.println("2: List my favorite foods");
+      System.out.println("3: Exit");
+      System.out.println();
+        selection = scanner.nextInt();
+      //selection = scanner.next();
+
+      //if(selection.equals("1"))
+      if (selection == 1)
+      System.out.println("Hello World!");
+
+      //if (selection.equals("2"))
+      if (selection == 2)
+      System.out.println("Apples, Bananas, Coconuts");
+
+      //if (selection.equals("3")){
+      if (selection == 3) {
+      System.out.println("Goodbye!");
+      break;
+      }
+    }
+
+      scanner.close();
+
+  }
+}
