2583b2e17bbf1e4a008a0ddcaa9410db5975d739
Miranda Martin <miranda@Mirandas-MacBook-Pro.local>
2018-02-21 10:22:01 -0700

    edits completed

--- JavaTheHardWay/SafeSquareRoot.java
+++ JavaTheHardWay/SafeSquareRoot.java
@@ -15,14 +15,14 @@ public class SafeSquareRoot {
       x = keyboard.nextDouble();
 
 
-    while (x < 0) {
-      System.out.println("I wont take the square root of a negative.");
-      System.out.println("\nNew number: ");
-      x = keyboard.nextDouble();
-    }
+      while (x < 0) {
+        System.out.println("I wont take the square root of a negative.");
+        System.out.println("\nNew number: ");
+        x = keyboard.nextDouble();
+      }
 
-    y = Math.sqrt(x);
-    System.out.println("The square root of " + x + " is " + y);
+      y = Math.sqrt(x);
+      System.out.println("The square root of " + x + " is " + y);
+    }
   }
-}
 }
